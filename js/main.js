5b9157c1c1ca9fbe23fc158fc07f444f1e402e74
Ivan Raichev <110658915+IvanRaichev@users.noreply.github.com>
2024-01-30 16:44:43 +0300

    upload file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.slider-person').slick({
+   infinite: true,
+   slidesToShow: 1,
+   slidesToScroll:1,
+   prevArrow: '<img class ="slider-arrows slider-arrows_left" src="./img/main/arrow-left.png" alt=""></img',
+   nextArrow: '<img class ="slider-arrows slider-arrows_right" src="./img/main/arrow-right.png" alt=""></img',
+ });
